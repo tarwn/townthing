2f55426aea4d68cac0002b4ba802c22313d1c2d5
Eli Weinstock-Herman <tarwn@tiernok.com>
2013-12-16 10:53:37 -0500

    buttons disabled during init, wider test map

--- town/js/src/townViewModel.js
+++ town/js/src/townViewModel.js
@@ -683,7 +683,7 @@ town.Terrain = {
     WATER:      { index: 0, symbol: '~', variants: 1, class: 'terrain-water', isWater: true, evaporation: 5 /* 5 inches/month */ },
     SAND:       { index: 1, symbol: '-', variants: 1, class: 'terrain-sand' },
     DIRT:       { index: 2, symbol: '=', variants: 3, class: 'terrain-dirt', canTransitionToGrass: true },
-    DRYGRASS:   { index: 4, symbol: '/', variants: 4, class: 'terrain-drygrass', canTransitionToGrass: true },
+    DRYGRASS: { index: 4, symbol: '/', variants: 4, class: 'terrain-drygrass', canTransitionToGrass: true, evaporation: .25 },
     GRASS:      { index: 5, symbol: '/', variants: 4, class: 'terrain-grass', supportsTrees: true, evaporation: .5 },
     ROCK:       { index: 6, symbol: '+', variants: 1, class: 'terrain-rock' },
     SWAMP:      { index: 7, symbol: ';', variants: 1, class: 'terrain-swamp' },
